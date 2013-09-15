4802a6358bc161f611c082349c266740efead706
Derek <dl91268n@pace.edu>
2013-09-15 19:15:01 -0400

    core.php changed a bit

--- app/Config/core.php
+++ app/Config/core.php
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubTwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVtUubTwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542296749683645');
+	Configure::write('Security.cipherSeed', '76859309657433542296749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
